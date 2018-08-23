1d7f41ff06312759919d51d09f668465274398bd
xinglinb <986294216@qq.com>
2018-08-23 11:23:54 +0800

    添加处理循环引用

--- deepClone.js
+++ deepClone.js
@@ -1,28 +1,39 @@
+/**
+ * deep clone
+ * @param  {[type]} obj object 需要进行克隆的对象
+ * @return {[type]}        深克隆后的对象
+ */
+
 const deepClone = (obj) => {
-    if (typeof obj !== 'object' && typeof obj !== 'function' || obj == null) {
-        return obj
+    let map = new WeakMap()
+    const _clone = (obj) => {
+        if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) {
+            return obj
+        }
+        let type = Object.prototype.toString.call(obj)
+        if (type == '[object Date]') return new Date(obj.getTime());
+        if (type == '[object Function]') {
+            let fn = new Function("return " + obj.toString())();
+            fn.prototype = obj.prototype;
+            return fn
+        };
+        if (type == '[object RegExp]') return new RegExp(obj.source, /\w*$/.exec(obj))
+        let child
+        if (map.has(obj)) {
+            return map.get(obj)
+        }
+        if (type == '[object Object]') {
+            proto = Object.getPrototypeOf(obj);
+            child = Object.create(proto);
+        } else if (Array.isArray(obj)) {
+            child = []
+        }
+        map.set(obj, child)
+        for (let i in obj) {
+            console.log(i)
+            child[i] = deepClone(obj[i])
+        }
+        return child
     }
-    let type = Object.prototype.toString.call(obj)
-    if (type == '[object Date]') return new Date(obj.getTime());
-    if (type == '[object Function]') return new Function("return " + obj.toString())();
-    if (type == '[object RegExp]') return new RegExp(obj.source, /\w*$/.exec(obj))
-    let child
-    if (type == '[object Object]') {
-        proto = Object.getPrototypeOf(obj);
-        child = Object.create(proto);
-    } else if (Array.isArray(obj)) {
-        child = []
-    }
-    for (let i in obj) {
-        console.log(i)
-        child[i] = deepClone(obj[i])
-    }
-    return child
+    return _clone(obj)
 }
-let a = {
-    a: function () {
-        this.a = 1
-    },
-    b: [1,2,3]
-}
-console.log(deepClone(a))
